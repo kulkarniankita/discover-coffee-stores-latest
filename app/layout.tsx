177fbe9e44ee420d109fc847a90e8657e23882dd
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2023-10-09 16:46:46 -0400

    undo local fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,11 @@
 import './globals.css';
 import type { Metadata } from 'next';
-// import { IBM_Plex_Sans } from 'next/font/google';
-import localFont from 'next/font/local';
+import { IBM_Plex_Sans } from 'next/font/google';
 
-// const ibmPlexSans = IBM_Plex_Sans({
-//   display: 'swap',
-//   subsets: ['latin'],
-//   weight: ['500', '600', '700'],
-// });
-
-const ibmPlexSans = localFont({
-  src: [
-    {
-      path: '../public/fonts/IBMPlexSans-Bold.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/IBMPlexSans-BoldItalic.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-  ],
+const ibmPlexSans = IBM_Plex_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
 });
 
 export const metadata: Metadata = {
