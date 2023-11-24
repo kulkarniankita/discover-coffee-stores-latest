b7472d0ae806f164d2809e558a3c535a00774a34
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2023-11-24 13:58:36 -0500

    Back to Home

---
+++ app/coffee-store/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Page(props: { params: { id: string } }) {
+  const {
+    params: { id },
+  } = props;
+  return (
+    <div className="h-full pb-80">
+      <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
+        <div className="mb-2 mt-24 text-lg font-bold">
+          <Link href="/">← Back to Home</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
