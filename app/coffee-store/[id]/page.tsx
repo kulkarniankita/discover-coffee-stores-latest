cf5a068d20dcf399928e163047846b95e26d47fd
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2023-12-01 05:56:02 -0500

    generateStaticParams

--- app/coffee-store/[id]/page.tsx
+++ app/coffee-store/[id]/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
-import { fetchCoffeeStore } from '@/lib/coffee-stores';
+import { fetchCoffeeStore, fetchCoffeeStores } from '@/lib/coffee-stores';
+import Image from 'next/image';
 
 async function getData(id: string) {
   //mapbox api
   return await fetchCoffeeStore(id);
+}
+
+export async function generateStaticParams() {
+  const coffeeStores = await fetchCoffeeStores();
+
+  return coffeeStores.map((coffeeStore) => ({
+    id: coffeeStore.slug,
+  }));
 }
 
 export default async function Page(props: { params: { id: string } }) {
@@ -14,13 +23,38 @@ export default async function Page(props: { params: { id: string } }) {
 
   const coffeeStore = await getData(id);
 
+  const { name = '', address = '', imgUrl = '' } = coffeeStore;
+
   console.log({ coffeeStore });
 
   return (
     <div className="h-full pb-80">
       <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
-        <div className="mb-2 mt-24 text-lg font-bold">
-          <Link href="/">← Back to Home</Link>
+        <div className="">
+          <div className="mb-2 mt-24 text-lg font-bold">
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className="my-4">
+            <h1 className="text-4xl">{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={740}
+            height={360}
+            className="max-h-[420px] min-w-full max-w-full rounded-lg border-2 sepia lg:max-w-[470px] "
+            alt={name}
+          />
+        </div>
+
+        <div className={`glass mt-12 flex-col rounded-lg p-4 lg:mt-48`}>
+          {address && (
+            <div className="mb-4 flex">
+              <p className="pl-2">{address}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
